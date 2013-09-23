f406139f61c629563aacbe8b829892b526734e5a
mattheweallen <mattheweallen@gmail.com>
2013-09-23 13:56:00 -0500

    Work on WebRequestFactory

--- WebServer/src/webserver/WebRequestFactory.java
+++ WebServer/src/webserver/WebRequestFactory.java
@@ -4,13 +4,37 @@
 package webserver;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
- * @author meallen
+ * The purpose of WebRequestFactory is to create a WebRequest object based on data from a BufferedReader. 
+ * 
+ * @author Matthew Allen
+ * @version September 23, 2013
  *
  */
 public class WebRequestFactory {
-	public static WebRequest createRequest(BufferedReader br) {
-		return null;
+	
+	/**
+	 * createRequest builds a WebRequest based on bufferedReader.
+	 * 
+	 * @param bufferedReader
+	 * @return
+	 * @throws IOException 
+	 */
+	public static WebRequest createRequest(BufferedReader bufferedReader) throws IOException {
+		WebRequest webRequest = new WebRequestImpl();
+		String sCurrentLine = bufferedReader.readLine();
+		String[] requestLine = sCurrentLine.split(" ");
+
+		webRequest.setMethod(requestLine[0].trim());
+		webRequest.setPath(requestLine[1].trim());
+		webRequest.setVersion(requestLine[2].trim());
+		
+		while(!(sCurrentLine = bufferedReader.readLine()).equals("")) {
+			String[] requestHeader = sCurrentLine.split(":");
+			webRequest.addHeader(requestHeader[0], requestHeader[0]);
+		}
+		return webRequest;
 	}
 }
